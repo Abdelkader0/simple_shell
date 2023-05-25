a4c2b2a5859a50c3425365e386515179a2f744cb
Abdelkader0 <ahmedbob43@gmail.com>
2023-05-24 17:45:02 -0700

    fix

--- main.h
+++ main.h
@@ -3,12 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <errno.h>
+
+extern char **environ; // Declare environ variable
 
 /* Function Declarations */
 char *_getline_command();
