449729288301bfc933660e692e4d22e3d1e37fcf
Abdelkader0 <ahmedbob43@gmail.com>
2023-05-25 15:10:10 -0700

    fix

--- shell.h
+++ shell.h
@@ -10,6 +10,14 @@
 
 #define MAX_COMMAND_LENGTH 100
 
+int _strcmp(char *s1, char *s2);
+size_t _strncmp(char *s1, char *s2, size_t n);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+
 void execute_command(char *command, char **env);
 char **split_command(char *command);
 void cleanup(char *command, char **user_command);
