97524f4b79b9a60f53c97644929340d26c1417ef
Abdelkader0 <ahmedbob43@gmail.com>
2023-05-24 19:39:49 -0700

    str

---
+++ _strncmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncmp - compare two strings up to a specified number of characters
+ * @s1: first string
+ * @s2: second string
+ * @n: number of characters to compare
+ * Return: difference in ASCII values of the first differing characters
+ */
+size_t _strncmp(char *s1, char *s2, size_t n)
+{
+	size_t i;
+
+	for (i = 0; i < n && s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return 0;
+}
