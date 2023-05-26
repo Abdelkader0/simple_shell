84e9bbd5c57dd3064d28aea273a11d0af07803be
Abdelkader0 <ahmedbob43@gmail.com>
2023-05-26 12:24:49 -0700

    last2

--- _getline_co.c
+++ _getline_co.c
@@ -7,17 +7,17 @@
  */
 char *_getline_command(void)
 {
-	char *lineptr = NULL;
-	size_t charter_user = 0;
+        char *lineptr = NULL;
+        size_t charter_user = 0;
 
-	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "#cisfun$ ", 0);
+        if (isatty(STDOUT_FILENO) && isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "#cisfun$ ", 10);
 
-	if (getline(&lineptr, &charter_user, stdin) == -1)
-	{
-		free(lineptr);
-		return (NULL);
-	}
+        if (getline(&lineptr, &charter_user, stdin) == -1)
+        {
+                free(lineptr);
+                return NULL;
+        }
 
-	return (lineptr);
+        return lineptr;
 }
