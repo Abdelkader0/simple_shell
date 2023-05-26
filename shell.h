791c67e0d4c1246b600271e1fb570755e6c7b782
Abdelkader0 <ahmedbob43@gmail.com>
2023-05-25 17:06:11 -0700

    fix

--- shell.h
+++ shell.h
@@ -27,8 +27,7 @@ char *_getline_command(void);
 void _getenv(char **env);
 char **_get_token(char *lineptr);
 void _exit_command(char **args, char *lineptr, int _exit);
-int _fork_fun(char **arg, char **av, char **env,
-char *lineptr, int np, int c);
+int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
 char *_strtok(char *str, const char *delim);
 
 void execute_command(char *command, char **env);
